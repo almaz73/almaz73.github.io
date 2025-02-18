c8441a2dafc6e9386f718aa809892a567a8b7d0c
almaz <almaz_73@mail.ru>
2025-02-18 10:47:40 +0300

    try5

---
+++ tg_wbFree/WBfreeStore.js
@@ -0,0 +1,52 @@
+const {createApp, ref, onMounted} = Vue
+
+createApp({
+    setup() {
+        const message = ref('Hello!')
+        const userName = ref('$$$$$$$$$$')
+        const tgparams = ref('$$')
+        const initData = ref('')
+        const ls = ref('')
+        let webApp = {}
+
+
+        onMounted(() => {
+             ls.value = localStorage.getItem('tgparams');
+             console.log('localstorage ', ls.value)
+
+            webApp = window.Telegram?.WebApp;
+            console.log('webApp', webApp)
+
+            userName.value = webApp.initDataUnsafe.user?.username || 'Unknown';
+//            webApp.showAlert(`Добро пожаловать, ${userName.value}`);
+            console.log('location.search=', location.search)
+            tgparams.value = location.search
+
+            const initData = new URLSearchParams(window.location.search);
+            console.log('===',initData.get('_ijt'))
+
+            webApp.ready();
+             // Получаем initData
+               initData.value = webApp.initData;
+
+            console.log('  initData.value=',  initData.value)
+
+//tgparams.value=333
+//            if(tgparams.value) localStorage.setItem('tgparams', tgparams.value);
+
+        });
+
+
+        function setMessage() {
+           console.log('val=', message.value)
+
+            webApp.postEvent('web_app_data_send', { data: 'your_data' });
+        }
+
+        function save(){
+            webApp.sendData('#######'+JSON.stringify(message.value));
+        }
+
+        return {message, setMessage, userName, save, tgparams, initData, ls}
+    }
+}).mount('#app')
