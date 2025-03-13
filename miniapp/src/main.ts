06aef889d5cf18ffcc22a4db4c9519947b96bbb5
almaz <almaz_73@mail.ru>
2025-03-13 13:53:07 +0300

    online

--- miniapp/src/main.ts
+++ miniapp/src/main.ts
@@ -2,11 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 
+// @ts-expect-error
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
 
 app.mount('#app')
--- miniapp/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="about">
-    <h1>Главная страница</h1>
-  </div>
-</template>
-
-<script setup>
-
-</script>
