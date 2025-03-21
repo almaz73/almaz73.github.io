95fc0924c815a301d4b63e4f3e2deb8b2d24ea9e
almaz <almaz_73@mail.ru>
2025-03-21 18:31:56 +0300

    стадии

--- miniapp/src/App.vue
+++ miniapp/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <component :is="game"></component>
 
-<!--  игры-->
-<!--  <button @click="setGame(1)">1</button>-->
-<!--  <button @click="setGame(2)">2</button>-->
-<!--  <button @click="setGame(3)">3</button>-->
+  <!--  игры-->
+  <!--  <button @click="setGame(1)">1</button>-->
+  <!--  <button @click="setGame(2)">2</button>-->
+  <!--  <button @click="setGame(3)">3</button>-->
 
 
-<!--<DevelopPart/>-->
+  <!--<DevelopPart/>-->
 
 </template>
 
@@ -18,14 +18,20 @@ import KrestikiNoliki from "@/games/KrestikiNoliki.vue";
 import RockPaperSScissors from "@/games/RockPaperSScissors.vue";
 import {UsefbStore} from "@/pinia/fbStore.ts";
 
-const fbStore=UsefbStore()
+const fbStore = UsefbStore()
 let game = shallowRef(RockPaperSScissors)
 
 function setGame(val) {
-  switch (val){
-    case 1: game.value = FireBaseCheck; break
-    case 2: game.value = KrestikiNoliki; break
-    case 3: game.value = RockPaperSScissors; break
+  switch (val) {
+    case 1:
+      game.value = FireBaseCheck;
+      break
+    case 2:
+      game.value = KrestikiNoliki;
+      break
+    case 3:
+      game.value = RockPaperSScissors;
+      break
   }
 }
 
@@ -36,16 +42,18 @@ onMounted(() => {
     fbStore.myId = webApp.initDataUnsafe.user?.id || 0
     fbStore.myName = webApp.initDataUnsafe.user?.username || ''
 
-    if(!fbStore.myId) setTimeout(()=>{
+    if (!fbStore.myId) setTimeout(() => {
       fbStore.myId = fbStore.myId || parseInt(String(Math.random() * 1000))
       fbStore.myName = fbStore.nickName || fbStore.myName || 'Имя_' + parseInt(String(Math.random() * 100))
 
-      fbStore.myId =953446309
-      fbStore.myId =261538035
+      if (location.host.includes('localhost')) {
+        // fbStore.myId =261538035
+        // fbStore.myId =953446309
+        fbStore.myId = 4444444
+      }
     }, 500)
   }
 })
-
 
 
 </script>
