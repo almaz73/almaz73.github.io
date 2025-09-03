77272616126cb8fafa8fa0ef6ed66bc4ccf405c4
almaz <almaz_73@mail.ru>
2025-09-03 13:34:36 +0300

    myGarazh

--- myGarazh/sw.js
+++ myGarazh/sw.js
@@ -1,31 +1,29 @@
-const CACHE_NAME = 'my-garage-v1';
+const CACHE_NAME = 'my-garage-v1'
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
-];
+	'/',
+	'/index.html',
+	'/about.html',
+	'/assets/style.css',
+	'/assets/icons/icon-192x192.png',
+	'/assets/icons/icon-512x512.png',
+]
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+self.addEventListener('install', function (event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			console.log('urlsToCache', urlsToCache)
+			return cache.addAll(urlsToCache)
+		})
+	)
+})
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			if (response) {
+				return response
+			}
+			return fetch(event.request)
+		})
+	)
+})
