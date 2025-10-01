8120a19ba9a5443f31d1a83ad2f7d0f85769a983
almaz <almaz_73@mail.ru>
2025-10-01 17:03:46 +0300

    rf nav_menu

--- autosite_dev/src/js/header-buttons.js
+++ autosite_dev/src/js/header-buttons.js
@@ -1,9 +1,12 @@
 let burger_menu = document.querySelector('.button__burger');
 let panel = document.querySelector('.main-nav');
-console.log('panel = ', panel);
+let buttonBurger = document.querySelector('.button__burger');
+
 
 burger_menu.addEventListener('click', () => {
   let isOpened = panel.style.transform === 'none';
   panel.style.transform = isOpened ? 'translateX(150vw)' : 'none';
   document.body.style.overflow = isOpened ? 'auto' : 'hidden';
+  buttonBurger.style.backgroundImage = isOpened ? 'url(src/images/burger.svg)' : 'url(src/images/close.svg)';
+  document.body.style.marginRight = isOpened ? '0' : '15px';
 });
