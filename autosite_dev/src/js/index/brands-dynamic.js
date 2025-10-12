d431bcdfc17b16c23790ef1b13cf5aa02b9bde19
almaz <almaz_73@mail.ru>
2025-10-12 10:01:54 +0300

    dynamic-brands

--- autosite_dev/src/js/index/brands-dynamic.js
+++ autosite_dev/src/js/index/brands-dynamic.js
@@ -3,121 +3,121 @@ document.addEventListener('DOMContentLoaded', () => {
   let brandDatas = [
     {
       "url": "/cars/vaz-lada/",
-      "imgSrc": "/src/icons/lada.png",
+      "imgSrc": "/icons/lada.png",
       "text": "ВАЗ (LADA)",
       "total": 470
     },
     {
       "url": "/cars/kia/",
-      "imgSrc": "/src/icons/kia.png",
+      "imgSrc": "/icons/kia.png",
       "text": "KIA",
       "total": 162
     },
     {
       "url": "/cars/hyundai/",
-      "imgSrc": "/src/icons/hyundai.png",
+      "imgSrc": "/icons/hyundai.png",
       "text": "HYUNDAI",
       "total": 136
     },
     {
       "url": "/cars/renault/",
-      "imgSrc": "/src/icons/renault.png",
+      "imgSrc": "/icons/renault.png",
       "text": "RENAULT",
       "total": 104
     },
     {
       "url": "/cars/chevrolet/",
-      "imgSrc": "/src/icons/chevrolet.png",
+      "imgSrc": "/icons/chevrolet.png",
       "text": "CHEVROLET",
       "total": 87
     },
     {
       "url": "/cars/volkswagen/",
-      "imgSrc": "/src/icons/volkswagen.png",
+      "imgSrc": "/icons/volkswagen.png",
       "text": "VOLKSWAGEN",
       "total": 86
     },
     {
       "url": "/cars/skoda/",
-      "imgSrc": "/src/icons/skoda.png",
+      "imgSrc": "/icons/skoda.png",
       "text": "SKODA",
       "total": 82
     },
     {
       "url": "/cars/nissan/",
-      "imgSrc": "/src/icons/nissan.png",
+      "imgSrc": "/icons/nissan.png",
       "text": "NISSAN",
       "total": 70
     },
     {
       "url": "/cars/ford/",
-      "imgSrc": "/src/icons/ford.png",
+      "imgSrc": "/icons/ford.png",
       "text": "FORD",
       "total": 48
     },
     {
       "url": "/cars/opel/",
-      "imgSrc": "/src/icons/opel.png",
+      "imgSrc": "/icons/opel.png",
       "text": "OPEL",
       "total": 44
     },
     {
       "url": "/cars/toyota/",
-      "imgSrc": "/src/icons/toyota.png",
+      "imgSrc": "/icons/toyota.png",
       "text": "TOYOTA",
       "total": 34
     },
     {
       "url": "/cars/mitsubishi/",
-      "imgSrc": "/src/icons/mitsubishi.png",
+      "imgSrc": "/icons/mitsubishi.png",
       "text": "MITSUBISHI",
       "total": 28
     },
     {
       "url": "/cars/mazda/",
-      "imgSrc": "/src/icons/mazda.png",
+      "imgSrc": "/icons/mazda.png",
       "text": "MAZDA",
       "total": 22
     },
     {
       "url": "/cars/chery/",
-      "imgSrc": "/src/icons/chery.png",
+      "imgSrc": "/icons/chery.png",
       "text": "CHERY",
       "total": 21
     },
     {
       "url": "/cars/gaz/",
-      "imgSrc": "/src/icons/gaz.png",
+      "imgSrc": "/icons/gaz.png",
       "text": "ГАЗ",
       "total": 20
     },
     {
       "url": "/cars/haval/",
-      "imgSrc": "/src/icons/haval.png",
+      "imgSrc": "/icons/haval.png",
       "text": "HAVAL",
       "total": 17
     },
     {
       "url": "/cars/honda/",
-      "imgSrc": "/src/icons/honda.png",
+      "imgSrc": "/icons/honda.png",
       "text": "HONDA",
       "total": 17
     },
     {
       "url": "/cars/datsun/",
-      "imgSrc": "/src/icons/datsun.png",
+      "imgSrc": "/icons/datsun.png",
       "text": "DATSUN",
       "total": 15
     },
     {
       "url": "/cars/geely/",
-      "imgSrc": "/src/icons/geely.png",
+      "imgSrc": "/icons/geely.png",
       "text": "GEELY",
       "total": 14
     },
     {
       "url": "/cars/peugeot/",
-      "imgSrc": "/src/icons/peugeot.png",
+      "imgSrc": "/icons/peugeot.png",
       "text": "PEUGEOT",
       "total": 12
     }
@@ -126,12 +126,10 @@ document.addEventListener('DOMContentLoaded', () => {
   let txt = `<div class='container main__top--block'>
     <h2>Топ 20 марок по продаже</h2>
 
-    <div class='page__flex'>
-        <div class='box'>
-            <ul>`
+    <div class='list_brands'>`
 
   brandDatas.forEach((el => {
-    txt += ` <li>
+    txt += ` <div>
               <a href='${el.urlc}'>
                   <span class='icon'>
                       <img src='${el.imgSrc}' alt='' class=' mw-100 d-block' loading='lazy'>
@@ -139,11 +137,10 @@ document.addEventListener('DOMContentLoaded', () => {
                   <span class='text'>${el.text}</span>
                   <span class='total'>${el.total}</span>
               </a>
-          </li>`
+          </div>`
   }))
 
-  txt += ` </ul></div>
-    </div>
+  txt += `</div>
 </div>`
 
   document.querySelector('#brands_dynamic').innerHTML = txt
