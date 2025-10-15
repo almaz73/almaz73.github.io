05462b34909b26b2eac085e21a0291b5f9cbb726
a.fayzrakhmanov <a.fayzrakhmanov@cartat.ru>
2025-10-15 09:25:43 +0300

    upd

--- autosite_dev/src/js/index/offers.js
+++ autosite_dev/src/js/index/offers.js
@@ -26,10 +26,10 @@ function createNode(item, N) {
                 </div>
                 <div class="cart__box--bottom">
                 <a href="#">
-                    <img src="/src/images/compare_cars.svg" alt=''>
+                    <img src="icons/compare_cars.svg" alt=''>
                 </a>
                 <a href="#">
-                    <img src="/src/images/penta.svg" alt=''>
+                    <img src="icons/penta.svg" alt=''>
                 </a>
             </div>
             </div>`
