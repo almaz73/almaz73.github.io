70978a6b5cfd169209a25254fcff825faa7f010c
almaz <almaz_73@mail.ru>
2025-10-19 18:17:26 +0300

    заказ в телеграм

---
+++ berries/JavaScript/order.js
@@ -0,0 +1,48 @@
+let oredering_buttons = document.querySelector('.oredering_buttons');
+let myOrders = document.querySelector('#myOrders');
+oredering_buttons.style.display = 'none';
+let meAllOrders = localStorage.getItem('meAllOrders');
+
+let OrederList = meAllOrders ? JSON.parse(meAllOrders) : []; // из локалстораж прежние заказы отображаю
+// OrederList= [{id:1233, name:'Торт очароывашка', received:true}, {id:33, name:'Треугольник'}, {id:554, name:'Зефир'}];
+
+function preOrder() {
+  oredering_buttons.style.display = 'block';
+}
+
+function noorder() {
+  oredering_buttons.style.display = 'none';
+}
+
+function order() {
+  let codeOrder = parseInt(Math.random() * 9999);
+  OrederList.push({ code: codeOrder, name: counterTitle[counter] , id: counter });
+
+
+  let botId = 'bot7957374061:AAE3bSimGQu4rLXfOk9lsUp143r3m_Qefek';
+  let chatId = '-4823194137';
+  let text = 'Заказ: ' + codeOrder + ' — ' + counterTitle[counter] + ' ';
+  let linkTelega = `https://api.telegram.org/${botId}/sendMessage?chat_id=${chatId}&parse_mode=HTML&text=${text}`;
+
+  fetch(linkTelega)
+    .then(response => response.json())
+    .then(json => {
+      console.log(' все будет хорошо > > > ' + json);
+      oredering_buttons.style.display = 'none';
+      localStorage.setItem('meAllOrders', JSON.stringify(OrederList));
+      showOrders();
+    });
+}
+
+function showOrders() {
+  myOrders.innerHTML = '';
+  OrederList.reverse();
+
+  if (OrederList.length) myOrders.innerHTML = '<h4>Ваши заказы</h4>';
+  OrederList.forEach(el => {
+    myOrders.innerHTML += '<div class="order_number" onclick="chosen('+el.id+')"> код:' + el.code + ' - ' + el.name + '</div>';
+  });
+}
+
+console.log('myOrders = ', myOrders);
+showOrders();
--- berries/JavaScript/scriptNew.js
+++ berries/JavaScript/scriptNew.js
@@ -1,26 +1,41 @@
-  let counter = 0;
-  let slideIndex = 0;
-  let images = document.querySelectorAll('.image')
-  let timer
-  images[0].style.display = 'block';
+let counter = 0;
+let counterTitle = ['Халва', 'Шокалад', 'Блины', 'Пряники'];
+let slideIndex = 0;
+let images = document.querySelectorAll('.image');
+let _50x50s = document.querySelectorAll('._50x50');
+console.log('_50x50s = ', _50x50s);
+let name = document.querySelector('#name_dish');
+let timer;
+images[0].style.display = 'block';
 
-  function go(val) {
-    images[counter].style.opacity = '0';
-    images[counter].style.opacity = '0';
-    images[counter].style.opacity = '0';
+function go(val) {
+  // clear()
+
+  counter += val;
+  if (counter < 0) counter = 3;
+  if (counter > 3) counter = 0;
+
+  chosen(counter);
+
+  clearTimeout(timer);
+  timer = setTimeout(() => go(val), 2000);
+
+}
+
+function clear() {
+  [0, 1, 2, 3].forEach(el => {
+    images[el].style.opacity = '0';
+    _50x50s[el].style.boxShadow = 'none';
+  });
+}
+
+function chosen(id) {
+  clear();
+  name.innerHTML = counterTitle[id];
+  images[id].style.opacity = '1';
+  _50x50s[id].style.boxShadow = '0 0 8px red inset';
+}
 
 
-    counter += val;
-    if (counter < 0) counter = 3;
-    if (counter > 3) counter = 0;
-
-    images[counter].style.opacity = '1';
-
-    clearTimeout(timer)
-    timer = setTimeout(()=>go(val), 2000)
-
-  }
-
-
-  go(1)
-  document.addEventListener('click', res=>clearTimeout(timer));
+go(1);
+document.addEventListener('click', () => clearTimeout(timer));
