266fde12bdb8d5d925c1797629469451db8b70d8
almaz <almaz_73@mail.ru>
2025-10-21 10:10:59 +0300

    bf

--- autosite_dev/src/js/main.js
+++ autosite_dev/src/js/main.js
@@ -1,11 +1,19 @@
 import '@/js/comb.js';
 import '@/js/global-buttons.js';
-import '@/js/photo-opener.js'
+import '@/js/photo-opener.js';
 
 
 // Обработчик ошибки 404
 // При переходе на неверный адрес перенаправляем пользователя на страницу 404
-window.addEventListener('DOMContentLoaded', function (e) {
-    let parts = e.target.URL.split('/')
-    if (parts[3] && !['privacy-policy', 'papka'].includes(parts[3])) window.location.href = '/404.html';
+window.addEventListener('DOMContentLoaded', function(e) {
+  let parts = e.target.URL.split('/');
+  console.log('parts = ', parts);
+  console.log('parts[3] = ', parts[3]);
+  let available = [
+    'index.html',
+    'privacy-policy',
+    'papka'
+  ];
+
+  if (parts[3] && !available.includes(parts[3])) window.location.href = '/404.html';
 });
