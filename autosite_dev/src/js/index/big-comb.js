4a734dce06c028a370671f65164dfa36ecb6c4a7
almaz73 <almaz_73@mail.ru>
2025-10-28 16:43:49 +0300

    комбобокс дороботка

--- autosite_dev/src/js/index/big-comb.js
+++ autosite_dev/src/js/index/big-comb.js
@@ -34,7 +34,10 @@ window.addEventListener('load', () => {
       let items_list = the_Items.map(item =>  '<div data-parent="'+comb_name+'">'+item+'</div>')
 
       let html = `<div class='big-combo' tabindex='1'>
-        <span class='big-comb__selected'>${the_Items.value || comb_name}</span>
+        <span class='big-comb__selected'>
+          <span class='big-comb__placeholder'>${the_Items.value || comb_name}</span>
+          <input class='big-comb__input' type="text">
+        </span>
         <img src='svg/arrow-down.svg' alt='arrow' loading='lazy' width='10' height='18'>            
         <div class='big_comb__items' onclick='big_comb_select(event)'>
             ${items_list.join('')}
@@ -42,6 +45,23 @@ window.addEventListener('load', () => {
     </div>`;
       comb.innerHTML = html;
     }
+
+    let bigCombo = comb.querySelector('.big-combo')
+    let bigCombItems = comb.querySelector('.big_comb__items')
+    let bigCombInput = comb.querySelector('.big-comb__input')
+    let bigCombPlaceholder = comb.querySelector('.big-comb__placeholder')
+    bigCombo.addEventListener('focus', ()=>{
+      bigCombItems.style.display = 'block'
+      bigCombInput.style.display = 'inline'
+      bigCombPlaceholder.style.display = 'none'
+      bigCombInput.focus()
+    })
+
+    bigCombInput.addEventListener('blur', ()=>{
+      bigCombItems.style.display = 'none'
+      bigCombInput.style.display = 'none'
+      bigCombPlaceholder.style.display = ''
+    })
   })
 });
 
